528de9c0308b7b671a1af2ae0d56522825848a51
WigiWigi1 <minlebaevr@gmail.com>
2025-09-02 15:07:58 +0200

    MVP: project from scratch
    * Have size selection
    * Palette examples
    * Download PNG/PDF
    * Mobile devices support
    * UI encacements
    * Bug-fixes

--- qrgen.py
+++ qrgen.py
@@ -1,41 +1,37 @@
 from flask import Flask, render_template, request, jsonify
 import qrcode
+from qrcode.image.pil import PilImage
 import base64
 from io import BytesIO
-import os
 
 app = Flask(__name__)
 
-
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('index.html')  # Render the main frontend page
+    return render_template("index.html")
 
-
-@app.route('/generate_qr', methods=['GET'])
+@app.route("/generate_qr", methods=["POST"])
 def generate_qr():
-    link = request.args.get('link', '')
+    data = request.json
+    link = data.get("link")
+    fill_color = data.get("fill_color", "black")
+    back_color = data.get("back_color", "white")
+
     if not link:
         return jsonify({"error": "No link provided"}), 400
 
-    # Generate QR code
     qr = qrcode.QRCode(
         version=1,
-        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        error_correction=qrcode.constants.ERROR_CORRECT_M,
         box_size=10,
         border=4,
     )
     qr.add_data(link)
     qr.make(fit=True)
 
-    # Convert QR code to base64
-    img = qr.make_image(fill_color="black", back_color="white")
-    buffered = BytesIO()
-    img.save(buffered, format="PNG")
-    img_str = base64.b64encode(buffered.getvalue()).decode('utf-8')
+    img: PilImage = qr.make_image(fill_color=fill_color, back_color=back_color)
+    buf = BytesIO()
+    img.save(buf, format="PNG")
+    img_str = base64.b64encode(buf.getvalue()).decode("utf-8")
 
     return jsonify({"qr_code": img_str})
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
